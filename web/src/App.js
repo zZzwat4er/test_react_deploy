345dc2e4a3a6667b74bc9fcf5b4ce98ea05e1d71
zZzwat4er <valenten2001@mail.ru>
2023-10-23 11:51:30 +0300

    Add webapp to the main repo

---
+++ web/src/App.js
@@ -0,0 +1,52 @@
+import { createContext, useEffect, useState } from 'react';
+import axios from 'axios';
+import { useThemeParams } from '@vkruglikov/react-telegram-web-app';
+import './App.css';
+
+import { TodoItem } from './components/TodoItem';
+import Todo from './models/Todo';
+import { useAuthContext } from './hooks/useAuthContext';
+
+export const todoContext = createContext();
+
+function App() {
+
+  let [state, setState] = useState([]);
+  let { user, _ } = useAuthContext();
+  let [sheme, themeParams] = useThemeParams();
+
+  useEffect(() => {
+    async function fetchTasks() {
+      const tasks = await axios.get(`https://odd-tan-ox-wig.cyclic.app/tasks/telegram/${user.id}`)
+        .catch(e => setState([]));
+      const tasksJson = await tasks.data
+      setState(tasksJson.map(e => Todo.from(e)));
+    }
+
+    if (user?.id) {
+      fetchTasks();
+    }
+  }, []);
+
+
+
+  return (
+    <div className="App" style={
+      {
+        backgroundColor: themeParams.bg_color,
+        textDecorationColor: themeParams.text_color
+      }
+    }>
+      <todoContext.Provider value={setState}>
+        {user ? <div>
+          {state.map((todo) => <TodoItem todo={todo} />)}
+        </div>
+          :
+          <h1>Could not get user ID</h1>
+        }
+      </todoContext.Provider>
+    </div>
+  );
+}
+
+export default App;
---
+++ web/src/components/TodoItem.js
@@ -0,0 +1,32 @@
+import React, { Component, useContext } from 'react'
+import Todo from '../models/Todo'
+import { useThemeParams } from '@vkruglikov/react-telegram-web-app'
+import axios, { HttpStatusCode } from 'axios';
+import { todoContext } from '../App';
+
+export function TodoItem({ todo }) {
+  let [sheme, params] = useThemeParams();
+  let setTodo = useContext(todoContext)
+
+  function deleteTodo() {
+    axios.delete(`https://odd-tan-ox-wig.cyclic.app/tasks/${todo.taskId}`)
+      .then(res => {
+        if (res.status !== HttpStatusCode.InternalServerError) {
+          setTodo(prev => prev.filter(e => e.taskId !== todo.taskId));
+        }
+      })
+      .catch(e => { });
+  }
+
+  return (
+    <div className='Todo-Item' style={{
+      background: params.button_color
+    }}>
+      <ul >
+        <li className='Main-Text'>{todo.message}</li>
+        <li onClick={deleteTodo}>{"\u274C"}</li>
+        {/* <li className='Status'>{todo.status}</li> */}
+      </ul>
+    </div>
+  )
+}
