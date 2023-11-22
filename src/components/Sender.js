809213701a09df43c557b53d198212b7393fef31
zZzwat4er <valenten2001@mail.ru>
2023-11-22 23:51:24 +0300

    v 1.0

--- src/components/Sender.js
+++ src/components/Sender.js
@@ -6,6 +6,6 @@ export default function Sender( params ) {
     }
 
     return (
-        <div>Sended by <a href={`https://t.me/${params.senderUrl}`}>{params.senderUrl}</a></div>
+        <div>Sent by <a href={`https://t.me/${params.senderUrl}`}>{params.senderUrl}</a></div>
     )
 }
